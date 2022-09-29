030b5c69b5ebec9873d280d2ba02f1adb0312dd5
kinseyvo <54725252+kinseyvo@users.noreply.github.com>
2022-09-29 00:17:48 -0700

    Create 5_8.py

---
+++ If/5_8.py
@@ -0,0 +1,7 @@
+usernames = ["Batman", "Flash", "Nightwing", "admin", "Deez"]
+
+for name in usernames:
+    if name == "admin":
+      print("Hello admin, would you like to see a status report?")
+    else:
+      print(f"Hello {name}, thank you for logging in again.")
