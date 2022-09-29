f03cb80873c47fcdab0ebf65c29468879180f8cf
kinseyvo <54725252+kinseyvo@users.noreply.github.com>
2022-09-29 00:19:34 -0700

    Create 6_1.py

---
+++ Dictionaries/6_1.py
@@ -0,0 +1,6 @@
+person_info = {'first_name': 'Bruce', 'last_name': 'Wayne', 'age': 35, 'city': 'Gotham'}
+
+print(person_info['first_name'])
+print(person_info['last_name'])
+print(person_info['age'])
+print(person_info['city'])
